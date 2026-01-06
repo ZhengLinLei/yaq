109ba08be9e1b5fad1b927dbf9ae0c88646dc03f
Zheng Lin Lei <zheng9112003@gmail.com>
2026-01-06 12:05:54 +0100

    Refactor click event handling for buttons

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let clickCount = 0;  // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-    "？你认真的吗…", 
+    "真不喜欢吗…", 
     "要不再想想？", 
     "不许选这个！ ", 
     "我会很伤心…", 
@@ -53,10 +53,11 @@ noButton.addEventListener("click", function() {
 yesButton.addEventListener("click", function() {
     document.body.innerHTML = `
         <div class="yes-screen">
-            <h1 class="yes-text">!!!喜欢你!! ( >᎑<)♡︎ᐝ</h1>
+            <h1 class="yes-text">!!!喜欢你yaq!! ( >᎑<)♡︎ᐝ</h1>
             <img src="images/hug.png" alt="拥抱" class="yes-image">
         </div>
     `;
 
     document.body.style.overflow = "hidden";
+
 });
